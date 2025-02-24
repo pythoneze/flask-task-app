8fe83ccc3321c7cb0186e034b4302204d6a63eb5
pythoneze <pythoneze@gmail.com>
2025-02-24 19:51:31 -0300

    Setting rquests POST & GET

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_scss import Scss
 from datetime import datetime
@@ -21,9 +21,24 @@ class MyTask(db.Model):
         return f'<Task {self.id}>'
 
 # roots to webpages
-@app.route('/')
+# home page
+@app.route('/', methods=["POST", "GET"])
 def home():
-    return render_template('index.html')
+    # add task
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            print(f'An error occurred: {e}')
+            return 'Failed to add task'
+    # see all current tasks
+    else:
+        tasks = MyTask.query.order_by(MyTask.date).all()
+        return render_template('index.html', tasks=tasks)
 
 # run the app
 if __name__ == '__main__':
