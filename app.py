ea1e4ae421b8831f37242b507e67d3fe57dcbd91
pythoneze <pythoneze@gmail.com>
2025-02-24 18:33:04 -0300

    Start setting template and static files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_scss import Scss
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
